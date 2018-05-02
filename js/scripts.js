e2c570a7112cffa08f75fb8bdeb54c61d2aff41e
Angela Wairimu <adelleciousm467@gmail.com>
2018-05-02 16:26:11 +0300

    add javascript styles to the file

--- js/scripts.js
+++ js/scripts.js
@@ -6,24 +6,8 @@ $(document).ready(function() {
     $("#result").text(result);
   });
 });
-
-// jQuery user interface logic here.
 var leapYear = function(year) {
-  if (year % 4 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
@@ -31,3 +15,30 @@ var leapYear = function(year) {
 };
 
 // jQuery user interface logic here.
+// business logic
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// user interface logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) { // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
